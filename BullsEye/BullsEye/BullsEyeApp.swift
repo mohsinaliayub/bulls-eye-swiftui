ffcf79f24a88a84427ad4c4630369470afbcc21b
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2025-09-23 14:05:37 +0200

    Add Xcode project

---
+++ BullsEye/BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Mohsin Ali Ayub on 23.09.25.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BullsEye/BullsEye/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BullsEye
+//
+//  Created by Mohsin Ali Ayub on 23.09.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
